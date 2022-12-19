417ca671d891026780c01c8ee1a620934b1c462a
23sharma6539 <120128628+23sharma6539@users.noreply.github.com>
2022-12-19 13:48:32 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
-﻿List<animal> Lion = new List<animal>();
+﻿/*
+ * Program Name: Animal Final Project
+ * Programmer: Sid Sharma 
+ * Date: 12/19/20
+ *
+ * Description: My code runs an animal named Jeff who is the color red using a C# class 
+ * and then putting that animal onto a list, writing the contents of that list.
+ */
+
+
+
+List<animal> Lion = new List<animal>();
 
 animal lion = new animal();
 lion.name = "jeff";
